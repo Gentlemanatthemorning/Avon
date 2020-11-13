59a3a7103cbbcfb53856b923b7dbc0820cb43e60
Sergey Kalchenko <sergei.alex.kalchenko@gmail.com>
2020-11-13 15:42:08 +0200

    test task

---
+++ src/slider.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+    var next = document.getElementById('nextPic');
+    var prev = document.getElementById('prevPic');
+    var array = [];
+    var allLi = document.querySelectorAll('.banner__slider li');
+    var show = 0;
+
+    allLi[show].classList.add('visible');
+
+    next.addEventListener('click', () => {
+        allLi[show].classList.remove('visible');
+        show++;
+        (show >= allLi.length) ?
+            show = 0:null;
+
+        allLi[show].classList.add('visible');
+    });
+    
+    prev.addEventListener('click', () => {
+        allLi[show].classList.remove('visible');
+        show--;
+        (show <0) ?
+            show = allLi.length -1:null;
+
+        allLi[show].classList.add('visible');
+    })
+});
+
