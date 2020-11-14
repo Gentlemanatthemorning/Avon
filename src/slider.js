8cb21de160dd6e5ab61b627e6aaf663cdae0eaa9
Sergey Kalchenko <sergei.alex.kalchenko@gmail.com>
2020-11-14 20:31:25 +0200

    optimize code, correct styles

--- src/slider.js
+++ src/slider.js
@@ -1,28 +1,25 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-    var next = document.getElementById('nextPic');
-    var prev = document.getElementById('prevPic');
-    var array = [];
-    var allLi = document.querySelectorAll('.banner__slider li');
-    var show = 0;
+  var next = document.getElementById("nextPic");
+  var prev = document.getElementById("prevPic");
+  var array = [];
+  var allLi = document.querySelectorAll(".banner_slider li");
+  var show = 0;
 
-    allLi[show].classList.add('visible');
+  allLi[show].classList.add("visible");
 
-    next.addEventListener('click', () => {
-        allLi[show].classList.remove('visible');
-        show++;
-        (show >= allLi.length) ?
-            show = 0:null;
+  next.addEventListener("click", () => {
+    allLi[show].classList.remove("visible");
+    show++;
+    show >= allLi.length ? (show = 0) : null;
 
-        allLi[show].classList.add('visible');
-    });
-    
-    prev.addEventListener('click', () => {
-        allLi[show].classList.remove('visible');
-        show--;
-        (show <0) ?
-            show = allLi.length -1:null;
+    allLi[show].classList.add("visible");
+  });
 
-        allLi[show].classList.add('visible');
-    })
+  prev.addEventListener("click", () => {
+    allLi[show].classList.remove("visible");
+    show--;
+    show < 0 ? (show = allLi.length - 1) : null;
+
+    allLi[show].classList.add("visible");
+  });
 });
-
